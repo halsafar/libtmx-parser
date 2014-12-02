7cdf00724ad74daa6babd30d226f8054eba77711
Stephen Damm <shinhalsafar@gmail.com>
2014-12-02 16:19:56 -0600

    - using static fixture

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,18 +1,35 @@
 #include "gtest/gtest.h"
 #include "../src/tmxparser.h"
 
-class TmxParseTest : public ::testing::Test {
- protected:
-  virtual void SetUp()
-  {
-	  _map = new tmxparser::TmxMap();
-	  tmxparser::parseFromFile("../test_xml_level.tmx", _map, "assets/textures/");
-  }
+class TmxParseTest : public ::testing::Test
+{
+protected:
+	static void SetUpTestCase()
+	{
+		_map = new tmxparser::TmxMap();
+		tmxparser::parseFromFile("../test_xml_level.tmx", _map, "assets/textures/");
+	}
 
-  // virtual void TearDown() {}
 
-  tmxparser::TmxMap* _map;
+	static void TearDownTestCase()
+	{
+		delete _map;
+		_map = NULL;
+	}
+
+
+	virtual void SetUp()
+	{
+
+	}
+
+	// virtual void TearDown() {}
+
+	static tmxparser::TmxMap* _map;
 };
+
+
+tmxparser::TmxMap* TmxParseTest::_map = NULL;
 
 
 TEST_F(TmxParseTest, MapNotNull)
@@ -31,7 +48,19 @@ TEST_F(TmxParseTest, MapProperties)
 	ASSERT_EQ(16, _map->height);
 	ASSERT_EQ(16, _map->tileWidth);
 	ASSERT_EQ(16, _map->tileHeight);
-	//ASSERT_EQ(16, _map->nextObjectId);
+}
+
+
+TEST_F(TmxParseTest, TilesetProperties)
+{
+	ASSERT_EQ(2, _map->tilesetCollection.size());
+	tmxparser::TmxTileset tileset = _map->tilesetCollection[0];
+	ASSERT_EQ(1, tileset.firstgid);
+	ASSERT_EQ("super_mario_one_tileset", tileset.name);
+	ASSERT_EQ(16, tileset.tileWidth);
+	ASSERT_EQ(16, tileset.tileHeight);
+
+
 }
 
 

b4c3b02e3ce01b6a194012588c7017b64ae95212
Stephen Damm <shinhalsafar@gmail.com>
2014-12-02 16:08:47 -0600

    - tests updated to test map properties

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,40 @@
 #include "gtest/gtest.h"
 #include "../src/tmxparser.h"
 
+class TmxParseTest : public ::testing::Test {
+ protected:
+  virtual void SetUp()
+  {
+	  _map = new tmxparser::TmxMap();
+	  tmxparser::parseFromFile("../test_xml_level.tmx", _map, "assets/textures/");
+  }
+
+  // virtual void TearDown() {}
+
+  tmxparser::TmxMap* _map;
+};
+
+
+TEST_F(TmxParseTest, MapNotNull)
+{
+	ASSERT_TRUE(_map != NULL);
+}
+
+
+TEST_F(TmxParseTest, MapProperties)
+{
+	ASSERT_TRUE(_map != NULL);
+	ASSERT_EQ("1.0", _map->version);
+	ASSERT_EQ("orthogonal", _map->orientation);
+	ASSERT_EQ("right-down", _map->renderOrder);
+	ASSERT_EQ(16, _map->width);
+	ASSERT_EQ(16, _map->height);
+	ASSERT_EQ(16, _map->tileWidth);
+	ASSERT_EQ(16, _map->tileHeight);
+	//ASSERT_EQ(16, _map->nextObjectId);
+}
+
+
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
